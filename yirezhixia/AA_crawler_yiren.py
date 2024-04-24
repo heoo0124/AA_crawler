9675fef6b515a680257d586573365bbafdc60793
heoo0124 <18826136729@163.com>
2024-04-24 14:13:56 +0800

    【问题描述】:https://www.ffmh123.com/book/3137 【相关单号】:N/a 【Owner】:AA

--- yirezhixia/AA_crawler_yiren.py
+++ yirezhixia/AA_crawler_yiren.py
@@ -26,7 +26,14 @@ for item in chapter_links:
     k=k+1                                      #遍历列表元素
     chapter_name = item.string                              #本话的名字
     # if(chapter_name == "1.姐姐1"):continue
-    if(k<200):continue
+    """ 
+    1、1-415(ok) 416(X) 1-546(pdf) 546-最新
+    2、485对应506 
+    3、490(X)
+    4、534对应521
+    5、416对应433
+    """
+    if(k<=433 or k>521):continue
     chapter_url = base_url + item['href']                   #本话的地址
     chapter_response = requests.get(chapter_url)
     # 解析HTML文档
@@ -45,17 +52,17 @@ for item in chapter_links:
                     img_url = img.attrs[key]
                     break
         #img_url = img.attrs['src'] if img.attrs['src'].find("http") == 1 else img.attrs['data-original']
-        print(img_url)
+        print(f'索引{k}_章节{chapter_name}_图{i} '+img_url)
         if(len(img_url)==0):
                 print("========")
         try:
             img_response = requests.get(img_url)
         except BaseException:
-            print("Error: 获取图片{chapter_name}_{i}.jpg失败")
-            img_response = requests.get(yiren_url)
-            filename = f'{chapter_path}/fail_{img_url}_{i}.jpg'
+            print(f'Error: 获取图片{chapter_name}_{i}.jpg失败')
+            #img_response = requests.get(yiren_url)
+            filename = f'{chapter_path}/{chapter_name}_{i}_失败.txt'
             with open(filename, 'wb') as f:
-                f.write(img_response.content)
+                f.write(img_url.encode('utf-8'))
             i=i+1
             continue
 
